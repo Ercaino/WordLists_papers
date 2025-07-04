d344901d9c64ba9adfdb5eb7d82e48685ec12724
Ercaino <86023301+Ercaino@users.noreply.github.com>
2025-07-04 23:37:45 +0000

    Add: view wordlists in arch/debian

---
+++ tools/wordlists.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Listing wordlists structure:"
+
+tree -L 2 /usr/share/wordlists
+
+# Make the script executable
+# sudo chmod +x /usr/local/bin/wordlists
+
+# Run wordlists_arch
+
+# Default: ls -la /usr/share/wordlists
